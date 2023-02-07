8012c288233e656f4589b1c5df8ca4ce293ccec8
Kevin Keppeler <Kevin.KeppelerRV@gmail.com>
2023-02-07 12:10:52 +0100

    removed test.mp3

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
 from pytube import YouTube
 import os
 
-def DownloadVideo(link):
-  youtubeObject = YouTube(link)
-  youtubeObject = youtubeObject.streams.get_highest_resolution()
-  try:
-    youtubeObject.download(output_path="./Downloads")
-  except:
-    print("There has been an error in downloading yout YouTube video!")
-  print("Your download was successful!")
-
-
-def DowloadAudio(link):
-  youtubeObject = YouTube(link)
-  youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
-  try:
-    out_file = youtubeObject.download(output_path="./Downloads")
-
-    base, ext = os.path.splitext(out_file)
-    new_file = base + ".mp3"
-    os.rename(out_file, new_file)
-  except:
-    print("There has been an error in downloading yout YouTube video!")
-  print("Your download was successful!")
-
-
 
 def Download(link, data):
   youtubeObject = YouTube(link)
@@ -55,20 +31,8 @@ def Download(link, data):
 
 
 
-
-
-
 link = input("YouTube URL: ")
 data = input("video/audio: ")
 
 
 Download(link, data)
-
-# if data == "video":
-#   DownloadVideo(link)
-
-# elif data == "audio":
-#   DowloadAudio(link)
-
-# else:
-#   print("Pleaser enter 'video' or 'audio'")
