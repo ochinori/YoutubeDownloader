e51f5e3c9f48fa69261aaac2378df49a7e6d6fd1
Kevin Keppeler <Kevin.KeppelerRV@gmail.com>
2023-02-07 10:12:24 +0100

    general downloader

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from pytube import YouTube
+
+def Dowload(link):
+  youtubeObject = YouTube(link)
+  youtubeObject = youtubeObject.streams.get_highest_resolution()
+  try:
+    youtubeObject.download()
+  except:
+    print("There has been an error in downloading yout YouTube video!")
+  print("Your download was successful!")
