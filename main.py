92de8f85b03821e1e7449958e1b9ec06d5ce3447
Kevin Keppeler <Kevin.KeppelerRV@gmail.com>
2023-02-08 09:18:15 +0100

    Benennung hinzugefügt

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from pytube import YouTube
 import os
 
 
-def Download(link, data):
+def Download(link, data, name):
   youtubeObject = YouTube(link)
   
   if data == "video":
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
-      youtubeObject.download(output_path="./Downloads")
+      out_file = youtubeObject.download()
+      base, ext = os.path.splitext(out_file)
+      new_file = name + ext
+      os.chdir('D:/Dev/YoutubeDownloader/Downloads')
+      os.rename(out_file, new_file)
     except:
       print("There has been an error in downloading your YouTube video!")
     print("Your download was successful!")
@@ -17,10 +21,11 @@ def Download(link, data):
   elif data == "audio":
     youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
     try:
-      out_file = youtubeObject.download(output_path="./Downloads")
+      out_file = youtubeObject.download()
 
       base, ext = os.path.splitext(out_file)
-      new_file = base + ".mp3"
+      new_file = name + ".mp3"
+      os.chdir('D:/Dev/YoutubeDownloader/Downloads')
       os.rename(out_file, new_file)
     except:
       print("There has been an error in downloading your YouTube audio!")
@@ -33,6 +38,7 @@ def Download(link, data):
 
 link = input("YouTube URL: ")
 data = input("video/audio: ")
+name = input("name: ")
 
 
-Download(link, data)
+Download(link, data, name)
